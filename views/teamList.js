1d8a951dcf91e05561540d40ee19a135c0716b80
blueStragglr <oortcloud1599@gmail.com>
2020-08-17 20:57:34 +0900

    Integrate Vue.js component at product page

---
+++ views/teamList.js
@@ -0,0 +1,205 @@
+const teamList = {
+  // 6기
+  6: {
+    1: {
+      title: '칵텐더',
+      description: `불친절한 칵테일 메뉴판은 이제 그만!
+            한잔한잔 비싼 칵테일, 그동안 취향에 맞지 않는 칵테일을 고르셨을때 굉장히 실망하셨죠?
+            칵텐더를 통해 칵테일 주문 전, 정말 내게 맞는 칵테일인지 확인해보세요!`,
+      member: {
+        React: '임태현',
+        Node: '김영재'
+      },
+      link: [
+        {
+          href: 'http://www.cocktender.fun/',
+          text: '웹페이지 열기'
+        }
+      ]
+    },
+    2: {
+      title: '행복해서 다행이야: 다행',
+      description: `행복해서 다행이야, 나만의 행복 ‘다행’입니다.
+            쉼 없이 바쁜 일상에 지친 현대인들이 일상의 사소한 행복을 찾고, 기록하고, 다시 꺼내보는 행복 보관함 웹 서비스입니다.
+            행복을 먹고 크는 나만의 조랭이를 성장시키고, 꾸미면서 조랭이와 함께 일상생활의 행복을 찾아가 봐요!`,
+      member: {
+        React: '강주희, 민지호, 신성환',
+        Django: '강민성, 장성은'
+      },
+      link: [
+        {
+          href: 'https://dahaeng.me/',
+          text: '웹페이지 열기'
+        }
+      ]
+    },
+    3: {
+      title: '마이펫밀리',
+      description: `애견호텔 안심예약 서비스입니다. 동물등록번호가 등록된 반려견만을 맡김으로써 호텔측은 파양에 대한 걱정을 줄일 수 있습니다.
+        견주는 여러 애견호텔들의 정보를 쉽게 확인할 수 있으며 다양한 후기들을 비교하여 더 나은 애견호텔을 예약할 수 있습니다.`,
+      member: {
+        'Node.js': '이유진, 부유림',
+        'Android': '한가희, 이예진'
+      },
+      link: [
+        {
+          href: 'https://play.google.com/sore/apps/details?id=com.habitbread.main',
+          text: '앱 다운로드'
+        }
+      ]
+    },
+    4: {
+      title: '습관빵',
+      description: `'습관빵'은 좋은 습관을 만들고 싶은 사용자들에게 도움을 주는 모바일 앱 서비스입니다.
+        다양한 빵을 만드는 '제빵사'처럼 여러분의 다양한 습관을 구워 좋은 습관을 완성할 수 있도록 도와줄 겁니다.
+        습관을 기록하고 한 눈에 알아 볼 수 있는 통계와 랭킹, 푸시 알림을 통한 리마인더 기능을 제공 받아보세요 !`,
+      member: {
+        'IOS': '김수용, 서재훈',
+        'Android': '김초희, 염준우',
+        'NodeJS': '이우원, 김건훈',
+        'Design': '김효신'
+      },
+      link: [
+        {
+          href: 'https://play.google.com/store/apps/details?id=com.habitbread.main',
+          text: '앱 다운로드'
+        }
+      ]
+    },
+    5: {
+      title: '입시달력: 입',
+      description: `
+        '입시달력: 입력'은 학부모를 위해 까다로운 입시 일정을 편하게 관리해 주는 서비스입니다.
+        대학별, 전형별, 학과별로 다른 입시 일정을 캘린더에 모아서 확인할 수 있습니다.`,
+      member: {
+        'Android': '김다희',
+        'Django': '김혜인, 최성우'
+      },
+      link: [
+        {
+          href: 'https://play.google.com/store/apps/details?id=com.output.ipsi',
+          text: '앱 다운로드'
+        }
+      ]
+    },
+    6: {
+      title: '플레이',
+      description: `축구 하고 싶을땐, 플레이지! PlayEasy는 축구/풋살을 하고 싶은 모든 사람들이 쉽고 편리하게 이용할 수 있는 매칭 모집/지원 플랫폼 입니다.  지금 바로 내가 원하는
+        경기에 참여하세요!`,
+      member: {
+        'Android': '이운기, 신재현',
+        'Deep Learning': '임진욱, 김현진, 허재',
+        'Node.js': '김용민, 임영철',
+        '운영진': '임규산, 김효신'
+      },
+      link: []
+    },
+    7: {
+      title: '레모',
+      description: `레모닝은 사용자 맞춤 모닝콜 전자상거래 플랫폼 입니다.
+        내가 듣고싶은 목소리와 문장으로 깨워줍니다.
+        커스터마이징할 수 있는 주문서를 제작해서
+        ‘자신만 가지고 있는 특별한 모닝콜도 사용할 수 있습니다.’
+        모닝콜 인기 항목들을 관련 음악 정보와 사용자 취향을 머신러닝 분석하여
+        보다 사용자에게 알맞은 상품을 추천 하기도 합니다.
+        매일 내가 원하는 목소리와 문장으로 일어날 수 있다고?!
+        내일은 누가 날 어떻게 깨워줄지 기대되지 않으신가요?`, member: {
+        'Android': '서창우, 조연우',
+        'Django': '유동관, 이창섭',
+        'Deep Learning': '손원주, 손주영, 윤재영',
+        'Design': '김가현',
+      },
+      link: [
+        {
+          href: '앱 다운로드',
+          text: 'https://bit.ly/3i3kZ1c'
+        }
+      ]
+    },
+    8: {
+      title: '눈송이',
+      description: `
+        눈송이는 성인용품 리뷰 플랫폼으로, 누구든 편안한 마음으로 들릴 수 있는 깨끗하고 친절한 공간입니다. 다양한 콘텐츠가 담길 실험실과, 따스한 이야기가 오갈 커뮤니티도 준비
+        중입니다. 올바른 성 문화 확산과 생생한 정보 공유를 위해 눈송이 팀은 차곡차곡 나아가려 합니다.`,
+      member: {
+        'React Native': '한지원, 고상원, 안다희',
+        'Django': '최지수, 임정택, 장지창',
+        'Design': '오승민'
+      },
+      link: [
+        {
+          href: 'https://play.google.com/store/apps/details?id=com.snowflake.safelove',
+          text: '안드로이드 다운로드'
+        },
+        {
+          href: 'https://play.google.com/store/apps/details?id=com.snowflake.safelove',
+          text: 'iOS 다운로드 (testflight)'
+        }
+      ]
+    },
+    9: {
+      title: 'fitple',
+      description: `
+        Fitple은 원하는 서비스를 함께 만들기 위해
+        기획자, 디자이너, 개발자들이 소통하는 플랫폼입니다.
+        만들고 싶은 서비스 아이디어를 제안하고 팀원들을 모집하거나,
+        관심있는 프로젝트에 지원하여 함께할 수 있습니다.
+        Fitple을 통해 나와 Fit이 맞는 사람들과 함께하세요!`,
+      member: {
+        'React': '정상협, 고기훈',
+        'Django': '안동원, 김시현, 백경준',
+        'Design': '김효신'
+      },
+      link: [
+        {
+          href: 'https://fit-ple.com',
+          text: '웹페이지 가기'
+        }
+      ]
+    },
+    10: {
+      title: 'tingting',
+      description: `
+        학교 인증 기반의 대학생 미팅 애플리케이션입니다. 친한 친구들, 혹은 새로운 사람들과 팀을 만든 후, 태그 필터링 기능을 활용하여 마음에 드는 이성팀을 손쉽게 찾아보세요!
+        그 다음, 하트를 보내 매칭을 시도해보세요! 상대 팀원이 만장일치로 하트를 수락하면, 매칭이 성사됩니다!`,
+      member: {
+          'Android': '최정우, 여혜민',
+        'iOS': '김선우',
+'Node.js': '양지원, 하지영, 강재윤',
+'QA': '김동훈',
+'Design': '김가현'
+      },
+      link: [
+        {
+          href: 'https://bit.ly/3gM1iu9',
+          text: '안드로이드 다운로드'
+        },
+        {
+          href: 'https://apple.co/3gO85Ua',
+          text: 'iOS 다운로드'
+        }
+      ]
+    },
+    11: {
+      title: 'COPL',
+      description: `
+        COPL은 웹사이트 기반 O2O 광고 플랫폼 서비스입니다. 웹사이트에 접속해 우리 동네 카페나 음식점에 광고를 신청해보세요! 전화 없이 클릭 몇 번만으로 원하는 장소,
+        원하는 시간에 합리적인 비용으로 손쉽게 광고를 송출할 수 있습니다.`,
+      member: {
+          'Android': '강찬',
+            'React': '정상협, 황희연',
+        'Node.js': '강재윤',
+        'Design': '오승민',
+
+      },
+      link: [
+        {
+          href: 'https://www.copl.kr/',
+          text: '소개 페이지'
+        }
+      ]
+    }
+  }
+}
+
+export default teamList
